5209d3aba7e4f51e54c10f970c5528f669ec6ea5
Yuliya Dovgun <dovgun.yuliya@gmail.com>
2023-02-14 21:24:40 +0300

    add CSS styles for hw3

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -40,6 +40,7 @@ const HW3 = () => {
     return (
         <div id={'hw3'}>
             <div className={s2.hwTitle}>Homework #3</div>
+            <hr/>
             {/*для автоматической проверки дз (не менять)*/}
 
             <div className={s2.hw}>
@@ -48,6 +49,7 @@ const HW3 = () => {
                     addUserCallback={addUserCallback}
                 />
             </div>
+            <hr/>
         </div>
     )
 }
