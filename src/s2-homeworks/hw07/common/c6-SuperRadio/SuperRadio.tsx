0040fba6b27e8282bb2a08d684225a1a325f3e55
Yuliya Dovgun <dovgun.yuliya@gmail.com>
2023-03-19 21:59:15 +0300

    hw7 (select and radio)

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,9 +1,4 @@
-import React, {
-    ChangeEvent,
-    InputHTMLAttributes,
-    DetailedHTMLProps,
-    HTMLAttributes,
-} from 'react'
+import React, {ChangeEvent, DetailedHTMLProps, HTMLAttributes, InputHTMLAttributes,} from 'react'
 import s from './SuperRadio.module.css'
 
 type DefaultRadioPropsType = DetailedHTMLProps<
@@ -35,12 +30,11 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // делают студенты
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
     const spanClassName = s.span + (spanProps?.className ? ' ' + spanProps.className : '')
-
     const mappedOptions: any[] = options
         ? options.map((o) => (
               <label key={name + '-' + o.id} className={s.label}>
@@ -48,6 +42,9 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
+                      checked={o.id === Number(value)}
+                      value={o.id}
+                      name={o.value}
                       // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
@@ -60,9 +57,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                   >
                       {o.value}
                   </span>
-              </label>
-          ))
-        : []
+              </label>)) : []
 
     return <div className={s.options}>{mappedOptions}</div>
 }
