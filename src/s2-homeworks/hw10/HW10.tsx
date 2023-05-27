662fa88ac848c58c05f5bd7e52b80f185c5f864d
Yuliya Dovgun <dovgun.yuliya@gmail.com>
2023-05-27 16:10:45 +0300

    hw10 (loader)

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -15,19 +15,22 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType, boolean>(store => store.loading.isLoading)
+    const dispatch = useDispatch()
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+        dispatch(loadingAC(true))
 
-        // setTimeout
+        setTimeout(() => {dispatch(loadingAC(false))}, 1500)
     }
 
     return (
         <div id={'hw10'}>
+            <hr/>
             <div className={s2.hwTitle}>Homework #10</div>
+            <hr/>
 
-            <div className={s2.hw}>
+            <div className={s2.hw} style={{paddingLeft: "70px"}}>
                 {isLoading ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,9 +1,15 @@
-const initState = {
+export type initStateType = {
+    isLoading: boolean
+}
+const initState: initStateType = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): initStateType => { // fix any
     switch (action.type) {
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading}
+        }
         // пишет студент  // need to fix
 
         default:
